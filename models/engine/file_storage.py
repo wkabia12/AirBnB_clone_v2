73ca427072d6fd141c14c65bac09bc4544d01881
Martin Mulama <martin.mulama@yahoo.com>
2023-08-22 12:05:19 +0000

    added delete feature and updated the public instance method all() in filestorage engine

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns a list of objects of one type of class"""
+        if cls is None:
+            return self.__objects
+        else:
+            new_filtered_dict = {}
+            for obj, key in self.__objects.items():
+                if isinstance(key, cls):
+                    new_filtered_dict[obj] = key
+        return new_filtered_dict
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +52,14 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Delete obj from __objects if it's inside"""
+        if obj is not None:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            if key in self.__objects:
+                del self.__objects[key]
+                self.save()
