19195c2c922e8ff7372903021abd60332b167dfb
wkabia12 <wkabia12@gmail.com>
2023-09-24 09:37:54 +0000

    fix

--- models/state.py
+++ models/state.py
@@ -5,12 +5,14 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
+
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
     else:
         @property
         def cities(self):
